03d55cb312cf2bb4ee26bc93d31438c41a31e103
KolosDan <dan.danya99@gmail.com>
2018-06-09 13:23:23 +0500

    fuck

--- eosGateway.js
+++ eosGateway.js
@@ -6,24 +6,33 @@ creator_public = 'EOS7JwDx3We4JmbcXC7UDL3hU2LM26GfYzwtkiRy7HmQEpoAM8oqX'
 create = {'name':'test1',
           'eos_obj':EOS({keyProvider: creator_key}, binaryen)}
 
-function createAccount(name){
+function errorHandler(e) {
+  console.log(e.message);
+
+  console.log('Error');
+}
+
+function checkName(name){
   return new Promise(function(resolve,reject){
-    create.getKeyAccounts({'public_key': creator_public}).then(user_name =>
-    {
-      Keygen.generateMasterKeys().then(keys => {
-      pubkey = keys.publicKeys.active
-      privkey = keys.privateKeys.active
-      create.transaction(tr => {
-        tr.newaccount({
-          creator: user_name.account_names[0],
-          name: name,
-          owner: pubkey,
-          active: pubkey
-        })
-      }).then(function(){resolve({'pubkey':pubkey,
-                 'privkey':privkey})})
+    create.eos_obj.getAccount(name).then(acc => resolve(acc), err => reject(err))
   })
- })
+}
+
+function createAccount(user_name){
+  return new Promise(function(resolve,reject){
+    Keygen.generateMasterKeys().then(keys => {
+    pubkey = keys.publicKeys.active
+    privkey = keys.privateKeys.active
+    create.eos_obj.transaction(tr => {
+      tr.newaccount({
+        creator: create.name,
+        name: user_name,
+        owner: pubkey,
+        active: pubkey
+      })
+    }).then(() => {resolve({'pubkey':pubkey,
+               'privkey':privkey})}, err=>{reject('hi')})
+})
 })
 }
 
@@ -49,6 +58,4 @@ function getBalance(obj, token_contract){
 function transfer(obj, to, amount, currency, memo){
   return new Promise(function(resolve,reject){
     obj.eos_obj.transfer(obj.name, to, amount +' '+ currency, memo).then(tx => {resolve(tx)})
-  }
-  )
-}
+  })}
